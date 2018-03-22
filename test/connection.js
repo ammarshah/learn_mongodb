cc8f788cfe2a6a574896ac8d027f34c4ce5da17c
Ammar Shah <ammarshah123@gmail.com>
2018-03-23 01:16:05 +0500

    Connect to the database before tests run

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-// Connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo');
 
-// Ensure the connection has been successfully made
-mongoose.connection.once('open', function() {
-    console.log('Connection has been made, now make fireworks...');
-}).on('error', function(error) {
-    console.log('Connection error:', error);
+// Connection to the database before tests run
+before(function(done) {
+    // Connect to mongodb
+    mongoose.connect('mongodb://localhost/testaroo');
+    
+    // Ensure the connection has been successfully made
+    mongoose.connection.once('open', function() {
+        console.log('Connection has been made, now make fireworks...');
+        done();
+    }).on('error', function(error) {
+        console.log('Connection error:', error);
+    });
 });
